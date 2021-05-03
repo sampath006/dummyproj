3b1d782b2bf824e17f80364f12668233f32a4a1e
Deepak_Kumar <deepakspidey123@gmail.com>
2021-05-03 18:45:11 +0530

    camps data dynamic

--- dummy/Camps/views.py
+++ dummy/Camps/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Camps
 
 # Create your views here.
 
 def camps(request):
     campsdata=Camps.objects.all()
-    return render(request,'1Camps1.html')
+    return render(request,'1Camps1.html',{'campsdata':campsdata})
 
