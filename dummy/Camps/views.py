07570531df440086f3f248c26db3011ffbc7ee73
Deepak_Kumar <deepakspidey123@gmail.com>
2021-05-06 12:17:12 +0530

    making changes in camps

--- dummy/Camps/views.py
+++ dummy/Camps/views.py
@@ -4,6 +4,10 @@ from .models import Camps
 # Create your views here.
 
 def camps(request):
-    campsdata=Camps.objects.all()
+    if 'date' in request.GET:
+        date=request.GET['date']
+        campsdata=Camps.objects.filter(date__icontains=date)
+    else:
+        campsdata=Camps.objects.all()
     return render(request,'1Camps1.html',{'campsdata':campsdata})
 
