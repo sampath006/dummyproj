22c93132b58994c31d0986c2f0210865ed08a344
sampath006 <sampathkumar321reddy@gmail.com>
2021-05-06 15:51:33 +0530

    removed media

--- dummy/dummy/settings.py
+++ dummy/dummy/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'camps',
         'USER':'postgres',
-        'PASSWORD':'deepak',
+        'PASSWORD':'saisai35',
         'HOST':'localhost',
     }
 }
@@ -131,7 +131,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-MEDIA_URLS='/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- dummy/dummy/urls.py
+++ dummy/dummy/urls.py
@@ -26,4 +26,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
