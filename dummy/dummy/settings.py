c6cb948450101f654659efdc02301408c12473de
sampath006 <sampathkumar321reddy@gmail.com>
2021-05-06 21:00:22 +0530

    authentication done

--- dummy/dummy/settings.py
+++ dummy/dummy/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Camps.apps.CampsConfig'
-]
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
