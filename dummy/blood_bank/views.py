32a532d4b699d77b1fa19ce32c61a3c38fa575be
Deepak_Kumar <deepakspidey123@gmail.com>
2021-05-07 10:01:25 +0530

    added search to blood bank

--- dummy/blood_bank/views.py
+++ dummy/blood_bank/views.py
@@ -3,6 +3,9 @@ from .models import Blood_Bank
 # Create your views here.
 
 def blood_bank(request):
-
-    blood_banksdata = Blood_Bank.objects.all()
+    if 'location' in request.GET:
+        location=request.GET['location']
+        blood_banksdata = Blood_Bank.objects.filter(Address__icontains=location)
+    else:
+        blood_banksdata = Blood_Bank.objects.all()
     return render(request,'1blood_bank1.html',{'blood_banksdata': blood_banksdata})
