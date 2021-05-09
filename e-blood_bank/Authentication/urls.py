74c32a988ea074d9e05b96484f6ac0ec673f0320
sampath006 <sampathkumar321reddy@gmail.com>
2021-05-09 14:52:49 +0530

    dash board

--- e-blood_bank/Authentication/urls.py
+++ e-blood_bank/Authentication/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('login/',views.login,name='login'),
     path('signup/',views.signup,name='signup'),
-    path('logout/',views.logout,name='logout')
+    path('logout/',views.logout,name='logout'),
+    path('dashboard/',views.dashboard,name='dashboard')
 ]
--- e-blood_bank/Authentication/views.py
+++ e-blood_bank/Authentication/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 # Create your views here.
-
+def dashboard(request):
+    return render(request,'dashboard.html')
 def login(request):
     if request.method=='POST':
         username=request.POST['username']
